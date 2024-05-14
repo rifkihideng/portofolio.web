a910b43a890e55e04b00336774c916f67c093279
Ki <105150994+rifkihideng@users.noreply.github.com>
2024-05-15 06:48:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var hamburger = document.querySelector(".hamb");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
+
+hamburger.addEventListener("click", function(){
+    navlist.classList.toggle('open');
+});
